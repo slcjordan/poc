1145d4b7b2746ba4de0e79cd60b6906e11d3a7c1
Jordan Crabtree <jordan.crabtree@route.com>
2021-12-05 21:24:22 -0700

    refactor assert

--- cmd/assert/main.go
+++ cmd/assert/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"go/ast"
@@ -95,44 +96,228 @@ func interfaceMethodName(typ string) string {
 	return fmt.Sprintf("Check%s", camel(arrayName(typ)))
 }
 
+type memberInitialization Field
+
+func (m memberInitialization) Read([]byte) (int, error) {
+	return 0, errors.New("not implemented")
+}
+
+func (m memberInitialization) WriteTo(w io.Writer) (int64, error) {
+	typ := camel(arrayName(m.Type))
+	name := camel(m.Name)
+	mw := mustWriter{w: w}
+	mw.Fprintf(`
+		%s: new%s(assertion),`, name, typ)
+	return mw.total, nil
+}
+
+type initialField Field
+
+func (initialField) Read([]byte) (int, error) {
+	return 0, errors.New("not implemented")
+}
+
+func (f initialField) WriteTo(w io.Writer) (int64, error) {
+	mw := mustWriter{w: w}
+	mw.Fprintf(`
+
+func(parent *%s) %s(checkers ...%s) *Assertion {
+	parent.%sCheckers = checkers
+	return parent.assertion
+}`, f.Parent, camel(f.Name), interfaceTypeName(f.Type), privateCamel(f.Name))
+	return mw.total, nil
+}
+
+type performCheck Field
+
+func (m performCheck) Read([]byte) (int, error) {
+	return 0, errors.New("not implemented")
+}
+
+func (field performCheck) WriteTo(w io.Writer) (int64, error) {
+	mw := mustWriter{w: w}
+	currName := "val." + field.Name
+	if isBasicType(field.Name) {
+		currName = field.Name + "(val)"
+	}
+	mw.Fprintf(`
+	for _, checker := range  parent.%sCheckers {
+		checker.%s(t, desc + ".%s", %s)
+	}`,
+		privateCamel(field.Name),
+		interfaceMethodName(field.Type),
+		field.Name,
+		currName,
+	)
+	return mw.total, nil
+}
+
+type AssertionField Field
+
+func (a AssertionField) WriteTo(w io.Writer) (int64, error) {
+	mw := mustWriter{w: w}
+	mw.Fprintf(`
+	%s %s`, camel(a.Parent), a.Parent)
+	return mw.total, nil
+}
+
+func (a AssertionField) Read([]byte) (int, error) {
+	return 0, errors.New("not implemented")
+}
+
+type AssertionInitialization Field
+
+func (a AssertionInitialization) WriteTo(w io.Writer) (int64, error) {
+	mw := mustWriter{w: w}
+	mw.Fprintf(`
+	assertion.%s = new%s(&assertion)`, a.Parent, a.Parent)
+	return mw.total, nil
+}
+
+func (a AssertionInitialization) Read([]byte) (int, error) {
+	return 0, errors.New("not implemented")
+}
+
+type MyType Field
+
+func (a MyType) Read([]byte) (int, error) {
+	return 0, errors.New("not implemented")
+}
+
+func (a MyType) WriteTo(w io.Writer) (int64, error) {
+	mw := mustWriter{w: w}
+	mw.Fprintf(`
+	return &assertion
+}`)
+	return mw.total, nil
+}
+
+type AssertionImplementation Field
+
+func (m AssertionImplementation) Read([]byte) (int, error) {
+	return 0, errors.New("not implemented")
+}
+
+func (m AssertionImplementation) WriteTo(w io.Writer) (int64, error) {
+	mw := mustWriter{w: w}
+	mw.Fprintf(`
+
+func (a *Assertion) %s(t *testing.T, desc string, val %s){
+	a.%s.%s(t, desc+" %s", val)
+}`, interfaceMethodName(m.Parent), maybeQualify(m.Parent), m.Parent, interfaceMethodName(m.Parent), m.Parent)
+	return mw.total, nil
+}
+
+type forwardCheck Field
+
+func (m forwardCheck) Read([]byte) (int, error) {
+	return 0, errors.New("not implemented")
+}
+
+func (field forwardCheck) WriteTo(w io.Writer) (int64, error) {
+	mw := mustWriter{w: w}
+	mw.Fprintf(`
+	parent.%s.%s(t, desc + ".%s", val.%s)`,
+		camel(field.Name),
+		interfaceMethodName(field.Type),
+		field.Name,
+		field.Name,
+	)
+	return mw.total, nil
+}
+
+type arrayTypeDef struct {
+	typ  string
+	next string
+}
+
+func (a arrayTypeDef) Read([]byte) (int, error) {
+	return 0, errors.New("not implemented")
+}
+
+func (a arrayTypeDef) WriteTo(w io.Writer) (int64, error) {
+	mw := mustWriter{w: w}
+	mw.Fprintf(`
+
+type %s struct {
+	assertion      *Assertion
+	lengthCheckers []IntChecker
+	nth            map[int]%s
+
+	ForEach %s
+}`, camel(arrayName(a.typ)), camel(arrayName(a.next)), camel(arrayName(a.next)))
+	mw.Fprintf(`
+
+func new%s(assertion *Assertion) %s {
+	return %s {
+		assertion: assertion,
+		nth:       make(map[int]%s),
+		ForEach:   new%s(assertion),
+	}
+}`, camel(arrayName(a.typ)), camel(arrayName(a.typ)), camel(arrayName(a.typ)), camel(arrayName(a.next)), camel(arrayName(a.next)))
+	mw.Fprintf(`
+
+func (a *%s) Nth(i int) %s {
+	prev, ok := a.nth[i]
+	if ok {
+		return prev
+	}
+	result := new%s(a.assertion)
+	a.nth[i] = result
+	return result
+}`, camel(arrayName(a.typ)), camel(arrayName(a.next)), camel(arrayName(a.next)))
+	mw.Fprintf(`
+
+func (a *%s) Length(checkers ...IntChecker) *Assertion {
+	a.lengthCheckers = checkers
+	return a.assertion
+}`, camel(arrayName(a.typ)))
+	mw.Fprintf(`
+
+func (a *%s) %s(t *testing.T, desc string, val %s) {
+	for _, checker := range a.lengthCheckers {
+		checker.CheckInt(t, desc+".length", len(val))
+	}
+	for i, checker := range a.nth {
+		checker.%s(t, desc+fmt.Sprintf("[%%d]", i), val[i])
+	}
+	for _, curr := range val {
+		a.ForEach.%s(t, desc+".ForEach", curr)
+	}
+}`,
+		camel(arrayName(a.typ)),
+		interfaceMethodName(a.typ),
+		maybeQualify(a.typ),
+		interfaceMethodName(a.next),
+		interfaceMethodName(a.next),
+	)
+	return mw.total, nil
+}
+
 // Implementations writes assertion structs.
 type Implementations []Field
 
+func (imp Implementations) Read([]byte) (int, error) {
+	return 0, errors.New("not implemented")
+}
+
 // WriteTo the given writer.
 func (imp Implementations) WriteTo(w io.Writer) (int64, error) {
-	fields := make([]Field, len(imp))
-	copy(fields, imp)
-	byName := func(i int, j int) bool { return fields[i].Name < fields[j].Name }
-	byIsBasicType := func(i int, j int) bool {
-		if isBasicType(fields[i].Type) == isBasicType(fields[j].Type) {
-			return false
-		}
-		return isBasicType(fields[i].Type)
-	}
-	byParent := func(i int, j int) bool { return fields[i].Parent < fields[j].Parent }
-	sort.SliceStable(fields, byName)
-	sort.SliceStable(fields, byIsBasicType)
-	sort.SliceStable(fields, byParent)
 	mw := mustWriter{w: w}
 
-	declarations := make(map[string][]Field)
 	seen := make(map[string]bool)
-	var parents []string
 
-	for _, field := range fields {
-		if !seen[field.Parent] {
-			parents = append(parents, field.Parent)
-			seen[field.Parent] = true
-		}
-		declarations[field.Parent] = append(declarations[field.Parent], field)
-	}
 	mw.Fprintf(`
 
 type Assertion struct {`)
+	var lastParent string
 
-	for _, parent := range parents {
-		mw.Fprintf(`
-	%s %s`, camel(parent), parent)
+	for _, field := range imp {
+		if field.Parent == lastParent {
+			continue
+		}
+		lastParent = field.Parent
+		AssertionField(field).WriteTo(w)
 	}
 
 	mw.Fprintf(`
@@ -140,29 +325,44 @@ type Assertion struct {`)
 
 func New() *Assertion {
 	var assertion Assertion`)
-	for _, parent := range parents {
-		mw.Fprintf(`
-	assertion.%s = new%s(&assertion)`, parent, parent)
+	lastParent = ""
+
+	for _, field := range imp {
+		if field.Parent == lastParent {
+			continue
+		}
+		lastParent = field.Parent
+		AssertionInitialization(field).WriteTo(w)
 	}
 	mw.Fprintf(`
 	return &assertion
 }`)
 
-	for _, parent := range parents {
-		mw.Fprintf(`
+	lastParent = ""
 
-func (a *Assertion) %s(t *testing.T, desc string, val %s){
-	a.%s.%s(t, desc+" %s", val)
-}`, interfaceMethodName(parent), maybeQualify(parent), parent, interfaceMethodName(parent), parent)
+	for _, field := range imp {
+		if field.Parent == lastParent {
+			continue
+		}
+		lastParent = field.Parent
+		AssertionImplementation(field).WriteTo(w)
 	}
+	lastParent = ""
 
-	for _, parent := range parents {
+	for _, f := range imp {
+		if f.Parent == lastParent {
+			continue
+		}
+		lastParent = f.Parent
 		mw.Fprintf(`
 
 type %s struct {
 	assertion *Assertion
-`, parent)
-		for _, field := range declarations[parent] {
+`, f.Parent)
+		for _, field := range imp {
+			if field.Parent != f.Parent {
+				continue
+			}
 			if isBasicType(field.Type) {
 				mw.Fprintf(`	%sCheckers []%s
 `, privateCamel(field.Name), interfaceTypeName(field.Type))
@@ -177,65 +377,48 @@ type %s struct {
 
 func new%s(assertion *Assertion) %s {
 	return %s {
-		assertion: assertion,`, parent, parent, parent)
-		for _, field := range declarations[parent] {
-			typ := camel(arrayName(field.Type))
-			name := camel(field.Name)
+		assertion: assertion,`, f.Parent, f.Parent, f.Parent)
+		for _, field := range imp {
+			if field.Parent != f.Parent {
+				continue
+			}
 			if isBasicType(field.Type) {
 				continue
 			}
-			mw.Fprintf(`
-		%s: new%s(assertion),`, name, typ)
+			memberInitialization(field).WriteTo(w)
 		}
 		mw.Fprintf(`
 	}
 }`)
-		for _, field := range declarations[parent] {
+		for _, field := range imp {
+			if field.Parent != f.Parent {
+				continue
+			}
 			if !isBasicType(field.Type) {
 				continue
 			}
-			mw.Fprintf(`
-
-func(parent *%s) %s(checkers ...%s) *Assertion {
-	parent.%sCheckers = checkers
-	return parent.assertion
-}`, parent, camel(field.Name), interfaceTypeName(field.Type), privateCamel(field.Name))
+			initialField(field).WriteTo(w)
 		}
 
 		mw.Fprintf(`
 
-func(parent *%s) %s(t *testing.T, desc string, val %s) {`, parent, interfaceMethodName(parent), maybeQualify(parent))
-		for _, field := range declarations[parent] {
-			if isBasicType(field.Type) {
-				currName := "val." + field.Name
-				if isBasicType(field.Name) {
-					currName = field.Name + "(val)"
-				}
-				mw.Fprintf(`
-	for _, checker := range  parent.%sCheckers {
-		checker.%s(t, desc + ".%s", %s)
-	}`,
-					privateCamel(field.Name),
-					interfaceMethodName(field.Type),
-					field.Name,
-					currName,
-				)
+func(parent *%s) %s(t *testing.T, desc string, val %s) {`, f.Parent, interfaceMethodName(f.Parent), maybeQualify(f.Parent))
+		for _, field := range imp {
+			if field.Parent != f.Parent {
 				continue
 			}
-			mw.Fprintf(`
-	parent.%s.%s(t, desc + ".%s", val.%s)`,
-				camel(field.Name),
-				interfaceMethodName(field.Type),
-				field.Name,
-				field.Name,
-			)
+			if isBasicType(field.Type) {
+				performCheck(field).WriteTo(w)
+				continue
+			}
+			forwardCheck(field).WriteTo(w)
 		}
 		mw.Fprintf(`
 }`)
 	}
 	seen = make(map[string]bool)
 
-	for _, field := range fields {
+	for _, field := range imp {
 		if !strings.HasPrefix(field.Type, "[") {
 			continue
 		}
@@ -246,121 +429,115 @@ func(parent *%s) %s(t *testing.T, desc string, val %s) {`, parent, interfaceMeth
 			}
 			seen[typ] = true
 			next := strings.Join(strings.Split(field.Type, "]")[dims:], "]")
-			mw.Fprintf(`
-
-type %s struct {
-	assertion      *Assertion
-	lengthCheckers []IntChecker
-	nth            map[int]%s
-
-	ForEach %s
-}`, camel(arrayName(typ)), camel(arrayName(next)), camel(arrayName(next)))
-			mw.Fprintf(`
-
-func new%s(assertion *Assertion) %s {
-	return %s {
-		assertion: assertion,
-		nth:       make(map[int]%s),
-		ForEach:   new%s(assertion),
-	}
-}`, camel(arrayName(typ)), camel(arrayName(typ)), camel(arrayName(typ)), camel(arrayName(next)), camel(arrayName(next)))
-			mw.Fprintf(`
-
-func (a *%s) Nth(i int) %s {
-	prev, ok := a.nth[i]
-	if ok {
-		return prev
-	}
-	result := new%s(a.assertion)
-	a.nth[i] = result
-	return result
-}`, camel(arrayName(typ)), camel(arrayName(next)), camel(arrayName(next)))
-			mw.Fprintf(`
-
-func (a *%s) Length(checkers ...IntChecker) *Assertion {
-	a.lengthCheckers = checkers
-	return a.assertion
-}`, camel(arrayName(typ)))
-			mw.Fprintf(`
-
-func (a *%s) %s(t *testing.T, desc string, val %s) {
-	for _, checker := range a.lengthCheckers {
-		checker.CheckInt(t, desc+".length", len(val))
-	}
-	for i, checker := range a.nth {
-		checker.%s(t, desc+fmt.Sprintf("[%%d]", i), val[i])
-	}
-	for _, curr := range val {
-		a.ForEach.%s(t, desc+".ForEach", curr)
-	}
-}`,
-				camel(arrayName(typ)),
-				interfaceMethodName(typ),
-				maybeQualify(typ),
-				interfaceMethodName(next),
-				interfaceMethodName(next),
-			)
+			arrayTypeDef{typ: typ, next: next}.WriteTo(w)
 		}
 	}
 	return mw.total, nil
 }
 
-// Field is a field in an object.
+// Field is a field in a struct.
+//
+// Name uses selector syntax for nested structs:
+//
+//   A {
+//   	B struct {
+//   		C int // Field { Parent: "A", Name: "A.B.C", Type: "int" }
+//   	}
+//   }
+//
+// Type uses array/slice declaration syntax:
+//
+// A {
+//  B []int // Field { Parent: "A", Name: "B", Type: "[]int" }
+//  C [2][]Person // Field { Parent: "A", Name: "C", Type: "[2][]Person"
+// }
+//
 type Field struct {
+	Parent string
 	Name   string
 	Type   string
-	Parent string
-	Len    int
 }
 
-func expr(parent string, name string, e ast.Expr) []Field {
-	curr := Field{Parent: parent, Name: name}
+func fieldFromIdent(parent string, name string, e *ast.Ident) Field {
+	curr := Field{Parent: parent, Name: name, Type: e.Name}
+	if name == "" {
+		curr.Name = curr.Type
+	}
+	return curr
+}
+
+func fieldFromSelectorExpr(parent string, name string, e *ast.SelectorExpr) Field {
+	return Field{Parent: parent, Name: name, Type: e.Sel.Name}
+}
+
+type arrayType struct {
+	Type   string
+	Length int
+}
+
+func (a arrayType) String() string {
+	if a.Length == 0 {
+		return "[]" + a.Type
+	}
+	return fmt.Sprintf("[%d]", a.Length) + a.Type
+}
+
+func mustAtoi(val string) int {
+	result, err := strconv.Atoi(val)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return result
+}
+
+func fieldsFromArray(parent string, name string, e *ast.ArrayType) []Field {
+	result := fieldsFromExpr(parent, name, e.Elt)
+	for i := range result {
+		a := arrayType{Type: result[i].Type}
+		if e.Len != nil {
+			a.Length = mustAtoi(e.Len.(*ast.BasicLit).Value)
+		}
+		result[i].Type = a.String()
+	}
+	return result
+}
+
+func fieldsFromStruct(parent string, name string, e *ast.StructType) []Field {
+	var result []Field
+	for _, field := range e.Fields.List {
+		currName := name
+		for _, n := range field.Names {
+			if currName != "" {
+				currName += "."
+			}
+			currName += n.Name
+		}
+		result = append(result, fieldsFromExpr(parent, currName, field.Type)...)
+	}
+	return result
+}
+
+func fieldsFromExpr(parent string, name string, e ast.Expr) []Field {
 	switch e.(type) {
 	case *ast.Ident:
-		curr.Type = e.(*ast.Ident).Name
-		if name == "" {
-			curr.Name = curr.Type
-		}
+		return []Field{fieldFromIdent(parent, name, e.(*ast.Ident))}
 	case *ast.SelectorExpr:
-		curr.Type = e.(*ast.SelectorExpr).Sel.Name
+		return []Field{fieldFromSelectorExpr(parent, name, e.(*ast.SelectorExpr))}
 	case *ast.ParenExpr:
-		return expr(parent, name, e.(*ast.ParenExpr).X)
+		return fieldsFromExpr(parent, name, e.(*ast.ParenExpr).X)
 	case *ast.StarExpr:
-		return expr(parent, name, e.(*ast.StarExpr).X)
+		return fieldsFromExpr(parent, name, e.(*ast.StarExpr).X)
 	case *ast.ArrayType:
-		result := expr(parent, name, e.(*ast.ArrayType).Elt)
-		for i := range result {
-			if e.(*ast.ArrayType).Len != nil {
-				length, err := strconv.Atoi(e.(*ast.ArrayType).Len.(*ast.BasicLit).Value)
-				if err != nil {
-					log.Fatal(err)
-				}
-				result[i].Type = fmt.Sprintf("[%d]", length) + result[i].Type
-			} else {
-				result[i].Type = "[]" + result[i].Type
-			}
-		}
-		return result
+		return fieldsFromArray(parent, name, e.(*ast.ArrayType))
 	case *ast.StructType:
-		var result []Field
-		for _, field := range e.(*ast.StructType).Fields.List {
-			currName := name
-			for _, fn := range field.Names {
-				if currName != "" {
-					currName += "."
-				}
-				currName += fn.Name
-			}
-			result = append(result, expr(parent, currName, field.Type)...)
-		}
-		return result
+		return fieldsFromStruct(parent, name, e.(*ast.StructType))
 	default:
 		log.Fatalf("%T is not supported", e)
 	}
-	return []Field{curr}
+	return nil
 }
 
-func fields(parent string, decls []ast.Decl) []Field {
+func fieldsFromFile(parent string, decls []ast.Decl) []Field {
 	var result []Field
 	for _, val := range decls {
 		decl, ok := val.(*ast.GenDecl)
@@ -372,9 +549,39 @@ func fields(parent string, decls []ast.Decl) []Field {
 			if !ok {
 				continue
 			}
-			result = append(result, expr(ts.Name.Name, "", ts.Type)...)
+			result = append(result, fieldsFromExpr(ts.Name.Name, "", ts.Type)...)
 		}
 	}
+	return result
+}
+
+var header = `// Code generated by cmd/assert; DO NOT EDIT.
+
+package assert
+
+import (
+	"testing"
+	"fmt"
+
+	"github.com/slcjordan/poc"
+)`
+
+func layout(fields []Field) []io.Reader {
+	byName := func(i int, j int) bool { return fields[i].Name < fields[j].Name }
+	byIsBasicType := func(i int, j int) bool {
+		if isBasicType(fields[i].Type) == isBasicType(fields[j].Type) {
+			return false
+		}
+		return isBasicType(fields[i].Type)
+	}
+	byParent := func(i int, j int) bool { return fields[i].Parent < fields[j].Parent }
+	sort.SliceStable(fields, byName)
+	sort.SliceStable(fields, byIsBasicType)
+	sort.SliceStable(fields, byParent)
+
+	var result []io.Reader
+	result = append(result, strings.NewReader(header))
+	result = append(result, Implementations(fields))
 	return result
 }
 
@@ -386,22 +593,12 @@ func parse(filename string) *ast.File {
 	return result
 }
 
-var header = []byte(`// Code generated by cmd/assert; DO NOT EDIT.
-
-package assert
-
-import (
-	"testing"
-	"fmt"
-
-	"github.com/slcjordan/poc"
-)`)
-
 func main() {
 	var filename string
 	flag.StringVar(&filename, "filename", "", "")
 	flag.Parse()
-	nodes := fields("", parse(filename).Decls)
+	objs := fieldsFromFile("", parse(filename).Decls)
+	readers := layout(objs)
 
 	base := filepath.Base(filename)
 	file, err := os.Create(filepath.Join("test/assert", base))
@@ -409,12 +606,10 @@ func main() {
 		log.Fatal(err)
 	}
 	defer file.Close()
-	_, err = file.Write(header)
-	if err != nil {
-		log.Fatal(err)
-	}
-	_, err = Implementations(nodes).WriteTo(file)
-	if err != nil {
-		log.Fatal(err)
+	for _, r := range readers {
+		_, err := io.Copy(file, r)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 }
